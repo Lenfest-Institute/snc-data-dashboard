feddc7df13676132c1757d2a1971b37240226a4f
tylermachado <3586466+tylermachado@users.noreply.github.com>
2024-08-06 14:36:47 -0400

    get routing working

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,19 @@
   export let data;
 </script>
 
+<header>
+  <h1>Filters</h1>
+</header>
+
+<nav>
+  <ul>
+    <li><a href="/data-revenue">Revenue</a></li>
+    <li><a href="/data-dei">DEI</a></li>
+    <li><a href="/data/audience">Audience</a></li>
+    <li><a href="/data-staffing">Staffing</a></li>
+  </ul>
+</nav>
+
 <main>
   <slot />
 </main>
