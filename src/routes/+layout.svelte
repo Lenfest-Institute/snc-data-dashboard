17121c2d5e9fb5b7e4542437be7e951cec72430d
tylermachado <3586466+tylermachado@users.noreply.github.com>
2024-08-06 16:13:19 -0400

    adjust generatation of nav links

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,15 +7,6 @@
   <h1>Filters</h1>
 </header>
 
-<nav>
-  <ul>
-    <li><a href="/data-revenue">Revenue</a></li>
-    <li><a href="/data-dei">DEI</a></li>
-    <li><a href="/data/audience">Audience</a></li>
-    <li><a href="/data-staffing">Staffing</a></li>
-  </ul>
-</nav>
-
 <main>
   <slot />
 </main>
---
+++ src/routes/data/+layout.svelte
@@ -0,0 +1,22 @@
+<script>
+  export let data;
+  import { page } from '$app/stores';
+  let currentPage = $page.url.pathname;
+  const pages = ['revenue', 'diversity', 'audience', 'staffing'];
+</script>
+
+<nav class="data__nav">
+  <ul>
+    {#each pages as page}
+      <li>
+        <a href={`/data/${page}`} class={currentPage === `/data/${page}` ? 'active' : ''}>
+          {page.charAt(0).toUpperCase() + page.slice(1)}
+        </a>
+      </li>
+    {/each}
+  </ul>
+</nav>
+
+<div class="data__wrapper">
+  <slot />
+</div>
