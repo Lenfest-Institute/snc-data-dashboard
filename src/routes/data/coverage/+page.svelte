d50b9472c3a5800f7ce9b5202e5be7c9db87c6b0
tylermachado <3586466+tylermachado@users.noreply.github.com>
2024-08-12 11:57:48 -0400

    add coverage section

---
+++ src/routes/data/coverage/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+  export let data = [];
+</script>
+
+<div class="charts__staffing">
+
+  <ul>
+    {#each data.props.data as row}
+      <li>{JSON.stringify(row)}</li>
+    {/each}
+  </ul>
+</div>
