d6cc908f433368db647aa25a59f1cae638804c80
tylermachado <3586466+tylermachado@users.noreply.github.com>
2024-08-23 16:20:38 -0400

    update config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,12 @@
-// @ts-check
 import { join } from 'path';
-
-// 1. Import the Skeleton plugin
 import { skeleton } from '@skeletonlabs/tw-plugin';
+import forms from '@tailwindcss/forms';
 
 /** @type {import('tailwindcss').Config} */
 export default {
-	// 2. Opt for dark mode to be handled via the class method
 	darkMode: 'class',
 	content: [
 		'./src/**/*.{html,js,svelte,ts}',
-		// 3. Append the path to the Skeleton package
 		join(require.resolve(
 			'@skeletonlabs/skeleton'),
 			'../**/*.{html,js,svelte,ts}'
@@ -20,8 +16,8 @@ export default {
 		extend: {},
 	},
 	plugins: [
-		// 4. Append the Skeleton plugin (after other plugins)
-		skeleton
+		forms,
+		skeleton // Skeleton must come last in this list.
 	]
 }
 						
