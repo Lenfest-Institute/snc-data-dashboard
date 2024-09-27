0c86cb9eee3ee7b09d450c8a5b7c5b1ed3c20baf
tylermachado <3586466+tylermachado@users.noreply.github.com>
2024-09-27 11:59:42 -0400

    work on staffing

--- src/routes/staffing/+page.svelte
+++ src/routes/staffing/+page.svelte
@@ -8,5 +8,28 @@
 </script>
 
 <div class="charts__wrapper charts__staffing">
-
+  <Chart
+    type={'beeswarm'}
+    title={'Total Editorial Staff'}
+    x={'count'}
+    z={'color'}
+    data={$filteredData.map(d => {
+      return {
+        color: d['Is PubMedia?'],
+        count: +d['Editorial Staff (FTE)']
+      };
+    })}
+  />
+  <Chart
+    type={'beeswarm'}
+    title={'Total Non-Editorial Staff'}
+    x={'count'}
+    z={'color'}
+    data={$filteredData.map(d => {
+      return {
+        color: d['Is PubMedia?'],
+        count: +d['Non-editorial Staff (FTE)']
+      };
+    })}
+    />
 </div>
