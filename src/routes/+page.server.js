20dc5e08c12fe28d4832ee4743a4e0132a724ce3
tylermachado <3586466+tylermachado@users.noreply.github.com>
2024-11-22 14:15:20 -0500

    reconfigure redirect for production

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,5 +1,5 @@
 import { redirect } from '@sveltejs/kit';
 
 export function load() {
-    throw redirect(307, '/audience');
+    throw redirect(307, process.env.NODE_ENV === 'production' ? '/snc-data-dashboard/audience' : '/audience');
 }
